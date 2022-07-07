7b183df2eed5a21669aa5a70704af3f8f666e676
Jaathavan <rjaathu@outlook.com>
2022-07-07 12:30:21 -0400

    create a working display function for the books

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let myLibrary = [];
+let myLibrary = ["H.P", "S", "K"];
 
-function Book() {
+function Book(author, title, pageCount) {
   // the constructor...
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+const library = document.querySelector('.books');
+
+function displayBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = document.createElement("div");
+        book.textContent = myLibrary[i];
+        library.appendChild(book).className = "book";
+    }
+}
+
+displayBooks();
